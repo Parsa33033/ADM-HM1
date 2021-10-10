f9183378679395465a08e08f2cd22d3359608823
unknown <parsa_h_m@outlook.com>
2021-10-10 17:35:04 +0330

    last commit

---
+++ problem1/python-functionals/validate-list-of-email-address-with-filter.py
@@ -0,0 +1,17 @@
+import re
+
+def fun(s):
+    return re.match("^[a-zA-Z][\w-]*@[A-Za-z0-9]+\.[a-zA-Z]{1,3}$", s)
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
